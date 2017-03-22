6433ac9d3d679927bc870716351ddde519354c8b
crisru <c.rubio17@usp.ceu.es>
2017-03-22 20:56:16 +0100

    more changes

--- src/transplants/db/jdbc/SQL_Hospital.java
+++ src/transplants/db/jdbc/SQL_Hospital.java
@@ -2,32 +2,89 @@ package transplants.db.jdbc;
 
 import java.io.*;
 import java.sql.*;
+import java.util.*;
 
-public class SQL_Insert_Hospital {
-	public static void main(String args[]) {
-		try {
-			// Open database connection
+import transplants.db.pojos.Hospital;
+
+public class SQL_Hospital {
+	Connection c;
+	List <Hospital> hospital= new ArrayList<Hospital>();
+	
+	private void SQL_Hospital(){
+		this.Connect();
+	}
+	
+	private void Connect(){
+		try{
 			Class.forName("org.sqlite.JDBC");
-			Connection c = DriverManager.getConnection("jdbc:sqlite:./db/transplant.db");//CAMBIARRRR, cambiado
+			this.c = DriverManager.getConnection("jdbc:sqlite:./db/transplant.db");//CAMBIARRRR, cambiado
 			c.createStatement().execute("PRAGMA foreign_keys=ON");
 			System.out.println("Database connection opened.");
-			
-			// Insert new record: begin
-			Statement stmt = c.createStatement();
-			String sql = "INSERT INTO Hospitals (name, phone_number, address, city"
-					+ "postcode, country) VALUES ('" + name + "', '" + phone_number + "',"
-					+ " '" + address + "', '" + city + "', '" + postcode + "', '" + country + "');";
-			
-			
-			stmt.executeUpdate(sql);
-			stmt.close();
-			System.out.println("Department info processed");
-			System.out.println("Records inserted.");
-			// Insert new record: end
-
-			
-		} catch (Exception e) {
+       }catch(Exception e){
 			e.printStackTrace();
 		}
 	}
+	
+	public void Disconnect(){
+		try{
+			c.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void Insert_Hospital(Hospital hospital){
+		try{
+			Statement stmt = c.createStatement();
+			String sql = "INSERT INTO Hospitals (name, phone_number, address, city"
+					+ "postcode, country) VALUES ('" + hospital.getName() + "', '" + hospital.getPhone_number() + "',"
+					+ " '" + hospital.getAddress() + "', '" + hospital.getCity() + "', '" + hospital.getPostcode() + "', '" + hospital.getCountry() + "');";
+			stmt.executeUpdate(sql);
+			stmt.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public List<Hospital> Search_Hospital(String name){
+		try{
+			Statement stmt = c.createStatement();
+			String sql = "SELECT * FROM HOSPITAL";
+			ResultSet rs = stmt.executeQuery(sql);
+			if {
+				//si coinciden los hospitales devolver el que estoy buscando
+			}else{
+				//si no coinciden devolver una lista vacia
+			}
+			rs.close();
+		    stmt.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		
+		}
+		//return //buscar todo entre los hospitales y devolver dentro del try y despues
+				//pasarle el string con loque quiere buscar
+	}
+		
+	public void Create_Table(){
+		try{
+		Statement stmt1 = c.createStatement();
+		String hospitals = "CREATE TABLE Hospitals "
+				   + "(id      		 	INTEGER  PRIMARY KEY AUTOINCREMENT,"
+				   + " name     		TEXT     NOT NULL, "
+				   + " phone_number		TEXT,  "
+				   + " address  		TEXT	 NOT NULL, "
+				   + " city 			TEXT,"
+				   + " postcode			TEXT,"
+				   + " country			TEXT)";
+		stmt1.executeUpdate(hospitals);
+		stmt1.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+					
+			
+						
+	
 }
--- src/transplants/db/ui/Insert_Hospitals.java
+++ src/transplants/db/ui/Insert_Hospitals.java
@@ -1,13 +1,66 @@
 package transplants.db.ui;
 
 import java.io.*;
+import java.util.List;
 
+import transplants.db.jdbc.SQL_Hospital;
 import transplants.db.pojos.Hospital;
 
 public class Insert_Hospitals {
-	public static void main(String args[]){
+	
+	
+	public static void main(String args[]){		
+		SQL_Hospital hospital= new SQL_Hospital();
 		try{
-			System.out.println("Please, input the hospital information: ");
+			BufferedReader console= new BufferedReader (new InputStreamReader (System.in));
+	        int opcion=0;
+	        while(true){
+	        	System.out.println("MENU: ");
+	        	System.out.println("1. Introduce a new hospital ");
+	        	System.out.println("2. Show the information about a specific hospital");
+	        	System.out.println("3. Log out ");
+	        	 do{
+	                    System.out.println("Introduzca una opcion [1-3]:");
+	                    String leido= console.readLine();
+	                    opcion= Integer.parseInt(leido);            
+	                }while (opcion<0|| opcion>3);
+	        	 switch (opcion){
+	        	 	case 1: 
+	        	 		Hospital hosp= Add_Hospital();
+	        	 		hospital.Insert_Hospital(hosp);
+	        	 	case 2: {
+	                      String name_hospital;
+	                        do{
+	                            System.out.println("Introduce the name of the hospital that you want: ");
+	                            name_hospital= console.readLine();
+	                            try{
+	                               List< Hospital> hospToShow= hospital.Search_Hospital(name_hospital);
+	                               //Hay que arreglar esto... pensarlo mañana en clase
+	                               printList(hospToShow);
+	                                break;
+	                            }catch (Exception ex){
+	                                ex.printStackTrace();
+	                            }
+	                        }while (name_hospital!=null);
+	                        break;
+	        	 	}
+	        	 	case 3:
+	        	 		System.exit(0);
+	        	 }
+	        }
+			
+		}catch(IOException ex){
+			ex.printStackTrace();
+		}
+	}
+	public static void printList(List <Hospital> hospToShow){
+		//printearlo como en el de bacterias desde el toString
+	}
+
+	public static Hospital Add_Hospital(){
+		Hospital hosp=null;
+		try{
+			System.out.println("Please, introduce the hospital information: ");
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			
 			System.out.print("Name: ");
@@ -28,9 +81,11 @@ public class Insert_Hospitals {
 			
 			System.out.print("Country: ");
 			String country = reader.readLine();
-			Hospital hospital= new Hospital(name, phone_number, address, city, post_code, country);
-		}catch(IOException ex){
+			hosp= new Hospital(name, phone_number, address, city, post_code, country);
+						
+		}catch(Exception ex){
 			ex.printStackTrace();
 		}
+		return hosp;
 	}
 }
