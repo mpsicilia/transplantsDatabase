588535482e9b65e423af65f88b4205b465a6f923
crisru <c.rubio17@usp.ceu.es>
2017-04-30 23:58:38 +0200

    todo jpa en el mismo project

---
+++ src/transplants/db/jpa/JPAhospital.java
@@ -0,0 +1,18 @@
+package transplants.db.jpa;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+import transplants.db.pojos.Hospital;
+
+public class JPAhospital {
+ public void create (Hospital hospital) throws IOException{
+	// Get the entity manager
+			EntityManager em = Persistence.createEntityManagerFactory("company-provider").createEntityManager();
+			em.getTransaction().begin();
+			em.createNativeQuery("PRAGMA foreign_keys=ON").executeUpdate();
+			em.getTransaction().commit();
+ }
+}
