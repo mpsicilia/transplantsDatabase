144ffc3689ad10ea7da5edf4895d252e51f63a68
mpsicilia <m.plaza6@usp.ceu.es>
2017-05-19 21:03:44 +0200

    errrorsssss when running

--- src/transplants/db/pojos/TransplantDatabase.java
+++ src/transplants/db/pojos/TransplantDatabase.java
@@ -3,13 +3,14 @@ package transplants.db.pojos;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlType;
-
+@Entity
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlRootElement(name = "TransplantDatabase")
 @XmlType(propOrder = { "hospitalsOfDatabase" })
