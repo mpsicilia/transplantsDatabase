88bf3d38412012b00a8d9348f41a73c4102812ec
crisru <c.rubio17@usp.ceu.es>
2017-06-01 21:36:38 +0200

    update hosp

--- src/transplants/db/jdbc/DBManager.java
+++ src/transplants/db/jdbc/DBManager.java
@@ -257,6 +257,10 @@ public class DBManager implements DBManagerInterface {
 	@Override
 	public boolean update(Object obj) {
 		try {
+			if (Hospital.class == obj.getClass()) {
+				Hospital hospi = (Hospital) obj;
+				return hosp.updateHospital(hospi);
+			}			
 			if (Doctor.class == obj.getClass()) {
 				Doctor doctor = (Doctor) obj;
 				return doct.updateDoctor(doctor);
